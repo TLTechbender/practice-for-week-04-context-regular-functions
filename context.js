db55713e4d1b97d0e2425c33fd83265e222ba9b2
TLTechbender <myjoyhasarrived2004@gmail.com>
2022-09-10 15:14:41 +0100

    Update: I need to study more and do more problem sets

--- context.js
+++ context.js
@@ -15,23 +15,23 @@ class Fish {
 const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
-
+ eat();
+/*Calling this.name evaluates to undefined*/
 
 /********************************* Scenario 2 *********************************/
-// nemo.eat = eat;
-// nemo.eat(); // ?
-
+ nemo.eat = eat;
+nemo.eat();
+/* I honestly don't know what is going to happen here*/
 
 /********************************* Scenario 3 *********************************/
-// nemo.eat = eat;
-// eat(); // ?
-
+ nemo.eat = eat;
+ eat(); // ?
+/*I don't get*/
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+ nemo.swim(); // Prints `Nemo swimmming in the water` as it is called using method invocation
 
 
 /********************************* Scenario 5 *********************************/
-// const swim = nemo.swim;
-// swim(); // ?
+const swim = nemo.swim;
+ swim();   /*I know that this is going to return an error, but I don't know what type of error*/
